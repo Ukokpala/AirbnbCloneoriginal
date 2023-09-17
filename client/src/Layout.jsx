30c179b145b6f698e21703b0f397b3a23de9d08e
ukokpala <okpalaukamaka480@gmail.com>
2023-09-17 21:00:16 +0100

    added login and register page

--- client/src/Layout.jsx
+++ client/src/Layout.jsx
@@ -3,10 +3,9 @@ import {Outlet} from "react-router-dom";
 
 export default function Layout() {
   return (
-    <div>
+    <div className="py-4 px-8 flex flex-col min-h-screen max-w-4xl mx-auto">
       <Header />
       <Outlet />
     </div>
   );
-
 }
