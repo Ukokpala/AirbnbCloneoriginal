351105e4e703acf9aacf973f0635b8b8413d4274
ukokpala <okpalaukamaka480@gmail.com>
2023-09-27 15:43:54 +0100

    working on register page

---
+++ api/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require("mongoose");
+require('dotenv').config()
+const app = express();
+
+app.use(express.json());
+app.use(cors({
+  credentials: true,
+  origin: 'http://127.0.0.1:5173',
+}));
+
+mongoose.connect('process.env.MONGO_URL');
+app.get('/test', (req,res) => {
+  res.json('test ok');
+});
+// EmmVI4aOmSFRW7tc
+app.post('/register', (req,res)=> {
+  const {name,email,password} = req.body;
+
+  res.json({name,email,password});
+});
+app.listen(4000);
