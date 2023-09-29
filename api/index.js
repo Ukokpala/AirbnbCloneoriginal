5afdd689d957e522c694eb77785d8ba97220d699
ukokpala <okpalaukamaka480@gmail.com>
2023-09-29 02:44:43 +0100

    added api using express

--- api/index.js
+++ api/index.js
@@ -1,23 +1,36 @@
 const express = require('express');
 const cors = require('cors');
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+const User = require('./models/User.js');
+const bcrypt = require('bcryptjs');
 require('dotenv').config()
 const app = express();
+
+const bcryptSalt = bcrypt.genSaltSync(10);
 
 app.use(express.json());
 app.use(cors({
   credentials: true,
-  origin: 'http://127.0.0.1:5173',
+  origin: 'http://localhost:5173',
 }));
 
-mongoose.connect('process.env.MONGO_URL');
+mongoose.connect(process.env.MONGO_URL);
 app.get('/test', (req,res) => {
   res.json('test ok');
 });
 // EmmVI4aOmSFRW7tc
-app.post('/register', (req,res)=> {
+app.post('/register', async(req,res)=> {
   const {name,email,password} = req.body;
+  try{
+    const userDoc = await User.create({
+      name,
+      email,
+      password:bcrypt.hashSync(password, bcryptSalt),
+    });
+  res.json(userDoc);
+  } catch (e) {
+    res.status(422).json(e);
+  }
 
-  res.json({name,email,password});
 });
 app.listen(4000);
