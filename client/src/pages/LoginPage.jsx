558ed2e140de940eeb66d5bcbad2f4c33b80a9bc
ukokpala <okpalaukamaka480@gmail.com>
2023-09-29 02:43:57 +0100

    done with register page

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,21 +1,30 @@
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
+import {useState} from "react";
 
-export default function LoginPage(){
+
+export default function LoginPage() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   return (
     <div className="mt-4 grow flex items-center justify-around">
       <div className="mb-64">
-      <h1 className="text-4xl text-center mb-4">Login</h1>
-      <form className="max-w-md mx-auto">
-        <input type="email" placeholder='your@email.com' />
-        <input type="password"  placeholder="password" />
-        <button className="primary">Login</button>
-        <div className="text-center py-2 text-gray-500">
-          Don't have account yet? <Link className="underline text-black" to={'/register'}>Register now</Link> 
-        </div>
-      </form>
+        <h1 className="text-4xl text-center mb-4">Login</h1>
+        <form className="max-w-md mx-auto" onSubmit={handleLoginSubmit}>
+          <input type="email"
+                 placeholder="your@email.com"
+                 value={email}
+                 onChange={ev => setEmail(ev.target.value)} />
+          <input type="password"
+                 placeholder="password"
+                 value={password}
+                 onChange={ev => setPassword(ev.target.value)} />
+          <button className="primary">Login</button>
+          <div className="text-center py-2 text-gray-500">
+            Don't have an account yet? <Link className="underline text-black" to={'/register'}>Register now</Link>
+          </div>
+        </form>
       </div>
-     
     </div>
   );
-} 
+}
