456d45e1efdbaa290bcba97fe629012ca681119b
ukokpala <okpalaukamaka480@gmail.com>
2023-09-30 13:03:01 +0100

    done with account login and acount page

---
+++ client/src/pages/AccountPage.jsx
@@ -0,0 +1,17 @@
+import { useContext } from "react";
+import { Navigate } from "react-router-dom";
+import { UserContext } from "../UserContext.jsx";
+
+export default function AccountPage() {
+  const {ready,user} = useContext(UserContext);
+
+  if (!ready) {
+    return 'Loading...';
+  }
+  if (ready && !user) {
+    return <Navigate to={'/login'} />
+  }
+  return(
+    <div>account page for {user.name}</div>
+  );
+}
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,11 +1,27 @@
-import {Link} from "react-router-dom";
-import {useState} from "react";
-
+import {Link, Navigate} from "react-router-dom";
+import {useContext, useState} from "react";
+import axios from "axios";
+import { UserContext } from "../UserContext";
 
 export default function LoginPage() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-
+  const [redirect, setRedirect] = useState(false);
+  const {setUser} = useContext(UserContext);
+  async function handleLoginSubmit(ev) {
+    ev.preventDefault();
+    try {
+      const {data} = await axios.post('/login', {email,password});
+      setUser(data);
+      alert('Login successfuil');
+      setRedirect(true);
+    } catch (e) {
+      alert('login failed');
+    }
+  }
+  if (redirect) {
+    return <Navigate to={'/'} />
+  }
   return (
     <div className="mt-4 grow flex items-center justify-around">
       <div className="mb-64">
