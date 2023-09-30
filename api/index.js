231b92b88f5114ebc46325ab71c5cb7c85b5adbf
ukokpala <okpalaukamaka480@gmail.com>
2023-09-30 13:02:13 +0100

    done with login page

--- api/index.js
+++ api/index.js
@@ -3,25 +3,29 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const User = require('./models/User.js');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 require('dotenv').config()
 const app = express();
 
 const bcryptSalt = bcrypt.genSaltSync(10);
+const jwtSecret = 'fasefraw4r5r3wq45wdfgw34twdfg';
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(cors({
   credentials: true,
-  origin: 'http://localhost:5173',
+  origin: true
 }));
 
 mongoose.connect(process.env.MONGO_URL);
 app.get('/test', (req,res) => {
   res.json('test ok');
 });
-// EmmVI4aOmSFRW7tc
+
 app.post('/register', async(req,res)=> {
   const {name,email,password} = req.body;
-  try{
+  try {
     const userDoc = await User.create({
       name,
       email,
@@ -32,5 +36,40 @@ app.post('/register', async(req,res)=> {
     res.status(422).json(e);
   }
 
+});
+
+app.post('/login', async (req,res) => {
+  const {email,password} = req.body;
+  const userDoc = await User.findOne({email});
+  if (userDoc) {
+    const passOk = bcrypt.compareSync(password, userDoc.password);
+    if (passOk) {
+      jwt.sign({
+        email:userDoc.email,
+        id:userDoc._id,
+      }, jwtSecret, {}, (err,token) => {
+        if (err) throw err;
+        res.cookie('token', token).json(userDoc);
+      });
+    } else {
+      res.status(422).json('pass not ok');
+    }
+  } else {
+    res.json('not found');
+  }
+});
+
+app.get('/profile', (req,res) => {
+  mongoose.connect(process.env.MONGO_URL);
+  const {token} = req.cookies;
+  if (token) {
+    jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+      if (err) throw err;
+      const {name,email,_id} = await User.findById(userData.id);
+      res.json({name,email,_id});
+    });
+  } else {
+    res.json(null);
+  }
 });
 app.listen(4000);
