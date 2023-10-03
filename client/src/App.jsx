da7ed0a1a8bb115593ec6eb13645b5bc377993a7
ukokpala <okpalaukamaka480@gmail.com>
2023-10-03 08:18:02 +0100

    fixed small bugs

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,10 @@ import Layout from './Layout';
 import RegisterPage from './pages/RegisterPage';
 import axios from 'axios';
 import { UserContextProvider } from './UserContext';
-import AccountPage from './pages/AccountPage.jsx';
+import ProfilePage from './pages/ProfilePage.jsx';
+import PlacesPage from './pages/PlacesPage';
+import PlacesFormPage from './pages/PlacesFormPage';
+
 
 axios.defaults.baseURL = 'http://localhost:4000';
 axios.defaults.withCredentials = true;
@@ -20,8 +23,10 @@ function App() {
         <Route index element={<IndexPage />} />
         <Route path="/login" element={<LoginPage/>} />
         <Route path="/register" element={<RegisterPage/>} />
-        <Route path="/account" element={<AccountPage/>} />
-        <Route path="/account/:subpage?" element={<AccountPage/>} />
+        <Route path="/account" element={<ProfilePage/>} />
+        <Route path="/account/places" element={<PlacesPage />} />
+        <Route path="/account/places/new" element={<PlacesFormPage />} />
+        <Route path="/account/places/:id" element={<PlacesFormPage />} />
         </Route>
       </Routes> 
     </UserContextProvider>
--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -2,14 +2,17 @@ import { useContext, useState } from "react";
 import { UserContext } from "../UserContext.jsx";
 import { Link, Navigate, useParams } from "react-router-dom";
 import axios from "axios";
+import PlacesPage from "./PlacesPage.jsx";
+import AccountNav from "../AccountNav.jsx";
 
-export default function AccountPage() {
+export default function ProfilePage() {
   const [redirect,setRedirect] = useState(null);
   const {ready,user,setUser} = useContext(UserContext);
   let {subpage} = useParams();
   if (subpage === undefined) {
    subpage = 'profile';
   }
+
   async function logout() {
   await axios.post('/logout');
   setUser(null);
@@ -24,23 +27,13 @@ export default function AccountPage() {
     return <Navigate to={'/login'} />
   }
 
- function linkClasses (type=null) {
-  let classes = 'py-2 px-6';
-  if(type === subpage) {
-    classes += ' bg-primary text-white rounded-full';
-  }
-  return classes;
- }
+ 
  if (redirect) {
   return <Navigate to={redirect} /> 
  }
   return(
     <div>
-      <nav className="w-full flex justify-center mt-8 gap-2 mb-8">
-        <Link className={linkClasses('profile')} to={'/account'}>My Profile</Link>
-        <Link className={linkClasses('bookings')} to={'/account/bookings'}>My Bookings</Link>
-        <Link className={linkClasses('places')} to={'/account/places'}>My Accomodations</Link>
-      </nav>
+      <AccountNav />
       {subpage === 'profile' && (
       <div className="text-center max-w-lg mx-auto">
       logged in as {user.name} ({user.email})<br />
@@ -48,6 +41,9 @@ export default function AccountPage() {
       </div>
      
     )} 
+    {subpage === 'places' && (
+      <PlacesPage />
+    )}
     </div>
   );
 }
