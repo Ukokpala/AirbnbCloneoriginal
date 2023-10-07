ec9661a227da87a4b842592d4af0f2d590a15cf8
ukokpala <okpalaukamaka480@gmail.com>
2023-10-07 08:18:26 +0800

    completed

--- api/index.js
+++ api/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const cors = require('cors');
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const User = require('./models/User.js');
 const Place = require('./models/Place.js');
+const Booking = require('./models/Booking.js');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
@@ -18,14 +19,24 @@ const bcryptSalt = bcrypt.genSaltSync(10);
 const jwtSecret = 'fasefraw4r5r3wq45wdfgw34twdfg';
 
 app.use(express.json());
-app.use('/uploads', express.static(__dirname+'/uploads'));
 app.use(cookieParser());
+app.use('/uploads', express.static(__dirname+'/uploads'));
 app.use(cors({
   credentials: true,
-  origin: true
+  origin: 'http://localhost:5173',
 }));
 
 mongoose.connect(process.env.MONGO_URL);
+
+function getUserDataFromReq(req) {
+  return new Promise((resolve, reject) => {
+    jwt.verify(req.cookies.token, jwtSecret, {}, async (err, userData) => {
+      if (err) throw err;
+      resolve(userData);
+    });
+  });
+}
+
 app.get('/test', (req,res) => {
   res.json('test ok');
 });
@@ -115,20 +126,72 @@ app.post('/places', (req,res) => {
   } = req.body;
  jwt.verify(token, jwtSecret, {}, async (err, userData) => {
   if (err) throw err;
-  const PlaceDoc =  await Place.create({
+  const placeDoc =  await Place.create({
     owner:userData.id,
     title,address,photos:addedPhotos,description,
-    perks,extraInfo,checkIn,checkOut,maxGuests,
+    perks,extraInfo,checkIn,checkOut,maxGuests,price,
   });
   res.json(placeDoc);
 });
 });
 
-app.get('/places', (req,res) => {
+app.get('/user-places', (req,res) => {
   const {token} = req.cookies;
   jwt.verify(token, jwtSecret, {}, async (err, userData) => {
     const {id} = userData;
-    res.json(await Place.find({owner:id}));
+    res.json( await Place.find({owner:id}) );
 });
 });
+
+app.get('/places/:id', async(req,res) => {
+const {id} = req.params;
+res.json(await Place.findById(id));
+});
+
+app.put('/places', async (req,res) => {
+  const {token} = req.cookies;
+  const {
+    id, title,address,addedPhotos,description,
+    perks,extraInfo,checkIn,checkOut,maxGuests,price,
+  } = req.body;
+  jwt.verify(token, jwtSecret, {}, async (err, userData) => {
+    if (err) throw err;
+    const placeDoc = await Place.findById(id);
+    if (userData.id === placeDoc.owner.toString()) {
+      placeDoc.set({
+        title,address,photos:addedPhotos,description,
+        perks,extraInfo,checkIn,checkOut,maxGuests,price,
+      });
+      await placeDoc.save();
+      res.json('ok');
+    }
+  });
+});
+
+
+app.get('/places', async (req,res) => {
+  res.json(await Place.find() );
+});
+
+app.post('/bookings', async (req, res) => {
+  const userData = await getUserDataFromReq(req);
+  const {
+    place,checkIn,checkOut,numberOfGuests,name,phone,price,
+  } = req.body;
+  Booking.create({
+    place,checkIn,checkOut,numberOfGuests,name,phone,price,
+    user:userData.id,
+  }).then((doc) => {
+    res.json(doc);
+  }).catch((err) => {
+    throw err;
+  });
+});
+
+
+app.get('/bookings', async (req,res) => {
+  const userData = await getUserDataFromReq(req);
+  res.json( await Booking.find({user:userData.id}).populate('place') );
+});
+
 app.listen(4000);
